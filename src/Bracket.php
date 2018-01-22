bb36f2af99fe0d4d3accfc35b793ce469a0e3e91
mrFanBender <habibullinrustam@yandex.ru>
2018-01-22 16:27:28 +0000

    initial commit

---
+++ src/Bracket.php
@@ -0,0 +1,27 @@
+<?php
+namespace Habibullin;
+
+class Bracket
+{
+	private $open_sk = 0;
+	private $close_sk = 0;
+	
+	public function compare(String $str){
+		$i=0;
+		$str = preg_replace("/[\t\r\n\s]+/", '', $str);	
+		while(isset($str[$i])){
+			if($str[$i]=='('){
+				$this->open_sk++;
+			}elseif($str[$i]==')'){
+				$this->close_sk++;
+			}else{
+				throw new InvalidArgumentException('Недопустимый символ в строке:'.$str[$i]);			
+			}
+			$i++;
+		}
+		if($this->open_sk == $this->close_sk)
+			return true;
+		return false;
+	} 
+}
+
